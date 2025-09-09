7de80683e365d211624f6a03feb92566f8ac3e19
Miguel Gamboa <miguelgamboa@outlook.com>
2025-09-09 12:12:35 +0100

    Lesson 02 - Manually resolving dependencies via constructor

--- lesson02-movies/src/main/kotlin/pt/isel/MovieLister.kt
+++ lesson02-movies/src/main/kotlin/pt/isel/MovieLister.kt
@@ -4,8 +4,8 @@ const val TOP_100_MOVIES =
     "https://gist.githubusercontent.com/fmcarvalho/" +
         "6d966b2d97d7b268102efa56dc00692c/raw/ffb6ebff59a1862eedf6b9856b0c92a7573d4cda/top_100_movies.csv"
 
-class MovieLister {
-    lateinit var finder: MovieFinder
+class MovieLister(val finder: MovieFinder) {
+    // lateinit var finder: MovieFinder
 
     /**
      * IoC => Inversion of Control:
