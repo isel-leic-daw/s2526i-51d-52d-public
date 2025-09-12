73aa620fcd57d09912ca9e2d87bf205fe441988b
Miguel Gamboa <miguelgamboa@outlook.com>
2025-09-12 12:10:01 +0100

    Lesson 03 - Add Jakarta Inject annotation to Domain

--- lesson02-movies/src/main/kotlin/pt/isel/DataSourceClientViaUrl.kt
+++ lesson02-movies/src/main/kotlin/pt/isel/DataSourceClientViaUrl.kt
@@ -2,6 +2,7 @@ package pt.isel
 
 import java.net.URI
 
+
 class DataSourceClientViaUrl : DataSourceClient {
     override fun load(path: String): Sequence<String> {
         return URI(path).toURL().openStream().bufferedReader().lineSequence()
