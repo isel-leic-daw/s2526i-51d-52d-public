a631d256529f3fa7a0124508f9d48a2f2f0214fb
Miguel Gamboa <miguelgamboa@outlook.com>
2025-09-18 18:08:40 +0100

    Lesson 06 - content-negotiation and Spring interceptors.

---
+++ lesson05-lab-spring-mvc/src/main/kotlin/pt/isel/demo/DemoApplication.kt
@@ -0,0 +1,24 @@
+package pt.isel.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@SpringBootApplication
+class DemoApplication
+
+@Configuration
+class DemoConfig(
+    private val intLogger: InterceptorLogger,
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        super.addInterceptors(registry)
+        registry.addInterceptor(intLogger)
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
