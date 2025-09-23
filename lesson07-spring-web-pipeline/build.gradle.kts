59309c68456f8f18e7c2adfd88c2e0dc67d13d22
Miguel Gamboa <miguelgamboa@outlook.com>
2025-09-23 12:00:03 +0100

    Lesson 07 - ArgumentResolver and HttpMessageConverter

--- lesson07-spring-web-pipeline/build.gradle.kts
+++ lesson07-spring-web-pipeline/build.gradle.kts
@@ -1,39 +1,33 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.5"
-    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "pt.isel"
-version = "0.0.1-SNAPSHOT"
-description = "Demo project for Spring Boot"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
+version = "unspecified"
 
 repositories {
     mavenCentral()
 }
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("net.glxn:qrgen:1.4")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
-}
-
-tasks.withType<Test> {
+tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
 }
