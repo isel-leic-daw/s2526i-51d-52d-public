279eea4640db5b10a134cb3218bb4ed5a9e6e7c3
Miguel Gamboa <miguelgamboa@outlook.com>
2025-09-25 11:26:02 +0100

    Lesson 07 - Example combining both Filter and Interceptor in Spring pipeline

--- lesson05-lab-spring-mvc/src/main/kotlin/pt/isel/demo/InterceptorLogger.kt
+++ lesson05-lab-spring-mvc/src/main/kotlin/pt/isel/demo/InterceptorLogger.kt
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.HandlerInterceptor
 
+const val CONTROLLER_INFO_KEY = "controllerInfoKey"
+
 @Component
 class InterceptorLogger : HandlerInterceptor {
     private val logger = LoggerFactory.getLogger(InterceptorLogger::class.java)
@@ -20,7 +22,8 @@ class InterceptorLogger : HandlerInterceptor {
             val controllerName = handler.beanType.simpleName
             val methodName = handler.method.name
 
-            logger.info("Handling request with controller=$controllerName, method=$methodName")
+            // logger.info("Handling request with controller=$controllerName, method=$methodName")
+            request.setAttribute(CONTROLLER_INFO_KEY, "controller=$controllerName, method=$methodName")
         }
 
         return super.preHandle(request, response, handler)
